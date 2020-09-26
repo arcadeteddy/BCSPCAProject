d701a0b85e569af0a0183444db87f1c0a34aa607
Theodore Lau <theodore.lau.888@gmail.com>
2020-09-26 11:28:08 -0700

    GitIgnore DS_Store

--- BCSPCAProject/MainViewController.swift
+++ BCSPCAProject/MainViewController.swift
@@ -12,7 +12,7 @@ import UIKit
 class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hello")
+        print("Hello World!")
     }
     
 }
