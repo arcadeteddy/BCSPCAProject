9aaf5f0f73e2c5e858ef32f02882a2e30093b7ad
Jasonhjkim <jasonhjkim1205@gmail.com>
2020-09-26 10:58:16 -0700

    made minor changes

---
+++ BCSPCAProject/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  BCSPCAProject
+//
+//  Created by work on 2020-09-26.
+//  Copyright © 2020 Theodore Lau. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
